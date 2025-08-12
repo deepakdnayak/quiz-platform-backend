e6fbca230c8699aae3ee5591a45d52db5d683e44
deepakdnayak <deepakdnayak2004@gmail.com>
2025-08-12 17:53:26 +0530

    Server Function Error Fix

--- api/server.js
+++ api/server.js
@@ -46,40 +46,4 @@ app.use('/api/admin', require('./routes/admin'));
 // Error handling middleware
 app.use(errorHandler);
 
-// Start server
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
-});
-
-
-// const Profile = require('./models/Profile');
-// const User = require('./models/User');
-
-
-
-// async function updateUsersWithProfile() {
-//   try {
-//     // await mongoose.connect('mongodb://localhost:27017/your_database', {
-//     //   useNewUrlParser: true,
-//     //   useUnifiedTopology: true,
-//     // });
-
-//     const profiles = await Profile.find();
-//     for (const profile of profiles) {
-//       await User.findByIdAndUpdate(profile.userId, { profile: profile._id });
-//     }
-//     console.log('Users updated with profile IDs');
-//     // mongoose.disconnect();
-//   } catch (err) {
-//     console.error('Error updating users:', err);
-//   }
-// }
-
-// updateUsersWithProfile()
+module.exports = app;
