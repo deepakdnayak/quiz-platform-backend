480768414532dc47a24ca7b930e67d35e444e1f6
deepakdnayak <deepakdnayak2004@gmail.com>
2025-08-12 18:00:52 +0530

    Modifications 4

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
+const connectDB = require('../config/db');
+const errorHandler = require('../middleware/error');
 const cors = require('cors');
 
 // Load environment variables
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: '../config.env' });
 
 // Connect to MongoDB
 connectDB();
@@ -36,12 +36,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Mount routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/users', require('./routes/users'));
-app.use('/api/quizzes', require('./routes/quizzes'));
-app.use('/api/students', require('./routes/students'));
-app.use('/api/instructors', require('./routes/instructors'));
-app.use('/api/admin', require('./routes/admin'));
+app.use('/api/auth', require('../routes/auth'));
+app.use('/api/users', require('../routes/users'));
+app.use('/api/quizzes', require('../routes/quizzes'));
+app.use('/api/students', require('../routes/students'));
+app.use('/api/instructors', require('../routes/instructors'));
+app.use('/api/admin', require('../routes/admin'));
 
 // Error handling middleware
 app.use(errorHandler);
